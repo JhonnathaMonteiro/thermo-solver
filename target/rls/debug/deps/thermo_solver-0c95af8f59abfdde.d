0395e827b127fe0a47ee7ef7d93116db60a62e78
Jhonnatha de Andrade Monteiro <jhonnatha@jhonnatha-mbp13.local>
2020-10-25 16:33:28 -0300

    first commit thermo-solver

---
+++ target/rls/debug/deps/thermo_solver-0c95af8f59abfdde.d
@@ -0,0 +1,5 @@
+/Users/jhonnatha/Projects/rust/thermo-solver/target/rls/debug/deps/thermo_solver-0c95af8f59abfdde.rmeta: src/lib.rs
+
+/Users/jhonnatha/Projects/rust/thermo-solver/target/rls/debug/deps/thermo_solver-0c95af8f59abfdde.d: src/lib.rs
+
+src/lib.rs:
