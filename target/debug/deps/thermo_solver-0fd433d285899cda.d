aba0616daabef44e3681ed7931a613c1afe7abb0
Jhonnatha de Andrade Monteiro <jhonnatha@jhonnatha-mbp13.local>
2020-10-25 16:45:58 -0300

    Untrack files in .gitignore

--- target/debug/deps/thermo_solver-0fd433d285899cda.d
+++
@@ -1,5 +0,0 @@
-/Users/jhonnatha/Projects/rust/thermo-solver/target/debug/deps/thermo_solver-0fd433d285899cda.rmeta: src/lib.rs
-
-/Users/jhonnatha/Projects/rust/thermo-solver/target/debug/deps/thermo_solver-0fd433d285899cda.d: src/lib.rs
-
-src/lib.rs:
